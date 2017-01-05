beccddf2330bc23fef89559ad9c98a6b8a2f9280
anoop.tomar@radisys.com <anoop.tomar@radisys.com>
2017-01-04 16:26:27 -0800

    Handling the UE Context Release Request with Cause User Inactivity
    Summary:
    As of now , in MME UE Context Release Request with Cause User Inactivity  is ignored . This is not a correct behavior . MME should handle it.
    
    Fix - Remove the check and remove the ignoring portion of logic in the code.
    
    Note - W/o this fix , MME will not respond with UE Context Release Command .
    
    Test Plan:
    1. Attach the UE
    2. Let inactivity timer expire in S1APteste or eNB( baicell or amarisoft ) .
    3. This would trigger S1AP: UE Context release Request message from S1APTester or eNB with cause : Radio Network: User inactivity
    
    Expected result - MME should not ignore this and should respond with UE Context Release Command.
    
    Signed-off-by: Anoop Tomar <anoop.tomar@radisys.com>

--- SRC/S1AP/s1ap_mme_handlers.c
+++ SRC/S1AP/s1ap_mme_handlers.c
@@ -632,6 +632,8 @@ s1ap_mme_handle_ue_context_release_request (
   S1ap_UEContextReleaseRequestIEs_t      *ueContextReleaseRequest_p = NULL;
   ue_description_t                       *ue_ref_p = NULL;
   MessageDef                             *message_p = NULL;
+  S1ap_Cause_PR                           cause_type;
+  long                                    cause_value;
   int                                     rc = RETURNok;
 
   OAILOG_FUNC_IN (LOG_S1AP);
@@ -641,18 +643,44 @@ s1ap_mme_handle_ue_context_release_request (
                       NULL, 0,
                       "0 UEContextReleaseRequest/%s enb_ue_s1ap_id " ENB_UE_S1AP_ID_FMT " mme_ue_s1ap_id " MME_UE_S1AP_ID_FMT " ",
                       s1ap_direction2String[message->direction], ueContextReleaseRequest_p->eNB_UE_S1AP_ID, ueContextReleaseRequest_p->mme_ue_s1ap_id);
+  // Log the Cause Type and Cause value
+  cause_type = ueContextReleaseRequest_p->cause.present;
 
-  /*
-   * The UE context release procedure is initiated if the cause is != than user inactivity.
-   * * * * TS36.413 #8.3.2.2.
-   */
-  if (ueContextReleaseRequest_p->cause.present == S1ap_Cause_PR_radioNetwork) {
-    if (ueContextReleaseRequest_p->cause.choice.radioNetwork == S1ap_CauseRadioNetwork_user_inactivity) {
-      OAILOG_DEBUG (LOG_S1AP, "UE_CONTEXT_RELEASE_REQUEST ignored, cause user inactivity\n");
-      MSC_LOG_EVENT (MSC_S1AP_MME, "0 UE_CONTEXT_RELEASE_REQUEST ignored, cause user inactivity", ueContextReleaseRequest_p->mme_ue_s1ap_id);
+  switch (cause_type) 
+  {
+    case S1ap_Cause_PR_radioNetwork:
+      cause_value = ueContextReleaseRequest_p->cause.choice.radioNetwork;
+      OAILOG_DEBUG (LOG_S1AP, "UE CONTEXT RELEASE REQUEST with Cause_Type = Radio Network and Cause_Value =", cause_value);
+      break;
+
+    case S1ap_Cause_PR_transport:
+      cause_value = ueContextReleaseRequest_p->cause.choice.transport;
+      OAILOG_DEBUG (LOG_S1AP, "UE CONTEXT RELEASE REQUEST with Cause_Type = Transport and Cause_Value = ", cause_value);
+      break;
+
+    case S1ap_Cause_PR_nas:
+      cause_value = ueContextReleaseRequest_p->cause.choice.nas;
+      OAILOG_DEBUG (LOG_S1AP, "UE CONTEXT RELEASE REQUEST with Cause_Type = NAS and Cause_Value = ", cause_value);
+      break;
+
+    case S1ap_Cause_PR_protocol:
+      cause_value = ueContextReleaseRequest_p->cause.choice.protocol;
+      OAILOG_DEBUG (LOG_S1AP, "UE CONTEXT RELEASE REQUEST with Cause_Type = Protocol and Cause_Value = ", cause_value);
+      break;
+
+    case S1ap_Cause_PR_misc:
+      cause_value = ueContextReleaseRequest_p->cause.choice.misc;
+      OAILOG_DEBUG (LOG_S1AP, "UE CONTEXT RELEASE REQUEST with Cause_Type = MISC and Cause_Value = ", cause_value);
+      break;
+
+    default:
+      OAILOG_ERROR (LOG_S1AP, "UE CONTEXT RELEASE REQUEST with Invalid Cause_Type = ", cause_type);
       OAILOG_FUNC_RETURN (LOG_S1AP, RETURNerror);
-    }
   }
+
+  /* Fix - MME shall handle UE Context Release received from the eNB irrespective of the cause. And MME should release the S1-U bearers for the UE and move the UE to ECM idle mode. 
+  Cause can influence whether to preserve GBR bearers or not.Since, as of now EPC doesn't support dedicated bearers, it is don't care scenario till we add support for dedicated bearers.
+  */ 
 
   if ((ue_ref_p = s1ap_is_ue_mme_id_in_list (ueContextReleaseRequest_p->mme_ue_s1ap_id)) == NULL) {
     /*
