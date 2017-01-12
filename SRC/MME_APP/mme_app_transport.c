adb68cf22652e587e8c8880ae537d653059612e4
gauthier <lionel.gauthier@eurecom.fr>
2017-01-12 15:31:24 +0100

    Bug trace

--- SRC/MME_APP/mme_app_transport.c
+++ SRC/MME_APP/mme_app_transport.c
@@ -67,21 +67,27 @@ int mme_app_handle_nas_dl_req (
   if (ue_context) {
     OAILOG_DEBUG (LOG_MME_APP, "DOWNLINK NAS TRANSPORT Found enb_ue_s1ap_id " ENB_UE_S1AP_ID_FMT "\n", ue_context->enb_ue_s1ap_id);
     enb_ue_s1ap_id = ue_context->enb_ue_s1ap_id;
+
+    if (!ue_context->is_s1_ue_context_release) {
+      NAS_DL_DATA_REQ (message_p).enb_ue_s1ap_id         = enb_ue_s1ap_id;
+      NAS_DL_DATA_REQ (message_p).ue_id                  = nas_dl_req_pP->ue_id;
+      NAS_DL_DATA_REQ (message_p).nas_msg                = nas_dl_req_pP->nas_msg;
+      nas_dl_req_pP->nas_msg                             = NULL;
+
+      MSC_LOG_TX_MESSAGE (MSC_MMEAPP_MME, MSC_S1AP_MME, NULL, 0,
+          "0 DOWNLINK NAS TRANSPORT enb_ue_s1ap_id " ENB_UE_S1AP_ID_FMT " ue id " MME_UE_S1AP_ID_FMT " ",
+          enb_ue_s1ap_id, nas_dl_req_pP->ue_id);
+
+      int to_task = (RUN_MODE_SCENARIO_PLAYER == mme_config.run_mode) ? TASK_MME_SCENARIO_PLAYER:TASK_S1AP;
+      rc = itti_send_msg_to_task (to_task, INSTANCE_DEFAULT, message_p);
+    } else {
+      OAILOG_DEBUG (LOG_MME_APP, "DOWNLINK NAS TRANSPORT stopped because of S1AP UE CONTEXT RELEASE procedure mme_ue_s1ap_id " MME_UE_S1AP_ID_FMT "\n", nas_dl_req_pP->ue_id);
+    }
+  } else {
+    OAILOG_DEBUG (LOG_MME_APP, "DOWNLINK NAS TRANSPORT failed mme_ue_s1ap_id " MME_UE_S1AP_ID_FMT " not found\n", nas_dl_req_pP->ue_id);
+
   }
 
-  if (!ue_context->is_s1_ue_context_release) {
-    NAS_DL_DATA_REQ (message_p).enb_ue_s1ap_id         = enb_ue_s1ap_id;
-    NAS_DL_DATA_REQ (message_p).ue_id                  = nas_dl_req_pP->ue_id;
-    NAS_DL_DATA_REQ (message_p).nas_msg                = nas_dl_req_pP->nas_msg;
-    nas_dl_req_pP->nas_msg                             = NULL;
-
-    MSC_LOG_TX_MESSAGE (MSC_MMEAPP_MME, MSC_S1AP_MME, NULL, 0,
-        "0 DOWNLINK NAS TRANSPORT enb_ue_s1ap_id " ENB_UE_S1AP_ID_FMT " ue id " MME_UE_S1AP_ID_FMT " ",
-        enb_ue_s1ap_id, nas_dl_req_pP->ue_id);
-
-    int to_task = (RUN_MODE_SCENARIO_PLAYER == mme_config.run_mode) ? TASK_MME_SCENARIO_PLAYER:TASK_S1AP;
-    rc = itti_send_msg_to_task (to_task, INSTANCE_DEFAULT, message_p);
-  }
   OAILOG_FUNC_RETURN (LOG_MME_APP, rc);
 }
 
