f3ee810891f9c57e94b958511d9b35e1d22f9deb
lionelgo <29477918+lionelgo@users.noreply.github.com>
2021-05-12 11:38:19 +0200

    Add AMBR parameters in entrypoint.sh and data_provisionning_users scripts Add missing Extended-***-***-BW-*L AVPs Enable build on ubuntu 20.04 + Extended-Max-Requested-Bandwidth-UL/DL AMBR 29.272 Add wait-for-it.sh
    Signed-off-by: lionelgo <29477918+lionelgo@users.noreply.github.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,8 +4,16 @@ set -euo pipefail
 
 HSS_DOMAIN=${HSS_FQDN#*.}
 HSS_HOSTNAME=${HSS_FQDN%%.*}
-ROAMING_ALLOWED=${ROAMING_ALLOWED:-true}
 MME_HOSTNAME=`echo $HSS_HOSTNAME | sed -e "s#hss#mme#"`
+# Defaults
+ROAMING_ALLOWED=${ROAMING_ALLOWED:-true}
+# Defaults for data_provisioning_users script
+# Values in bits per second
+UE_AMBR_UL=${UE_AMBR_UL:-20000000}
+UE_AMBR_DL=${UE_AMBR_DL:-40000000}
+APN1_AMBR_UL=${APN1_AMBR_UL:-10000000}
+APN1_AMBR_DL=${APN1_AMBR_DL:-20000000}
+
 
 CONFIG_FILES=`ls /openair-hss/etc/*.conf /openair-hss/etc/hss*json`
 
@@ -36,7 +44,8 @@ mkdir -p /openair-hss/logs
 
 # Create the certificates
 pushd /openair-hss/scripts
-./data_provisioning_users --apn ${APN1} --apn2 ${APN2} --key ${LTE_K} --imsi-first ${FIRST_IMSI} --msisdn-first 00000001 --mme-identity oai-mme.${REALM} --no-of-users ${NB_USERS} --realm ${REALM} --truncate False --verbose True --cassandra-cluster ${cassandra_Server_IP}
+./wait-for-it.sh  ${cassandra_Server_IP}:9042
+./data_provisioning_users --ue-ambr-max-requested-bandwidth-ul ${UE_AMBR_UL} --ue-ambr-max-requested-bandwidth-dl ${UE_AMBR_DL} --apn ${APN1} --apn1-ambr-max-requested-bandwidth-dl ${APN1_AMBR_DL} --apn1-ambr-max-requested-bandwidth-ul ${APN1_AMBR_UL} --apn2 ${APN2} --key ${LTE_K} --imsi-first ${FIRST_IMSI} --msisdn-first 00000001 --mme-identity oai-mme.${REALM} --no-of-users ${NB_USERS} --realm ${REALM} --truncate False --verbose True --cassandra-cluster ${cassandra_Server_IP}
 ./data_provisioning_mme --id 3 --mme-identity ${MME_HOSTNAME}.${REALM} --realm ${REALM} --ue-reachability 1 --truncate False  --verbose True -C ${cassandra_Server_IP}
 ./make_certs.sh ${HSS_HOSTNAME} ${REALM} ${PREFIX}
 popd
