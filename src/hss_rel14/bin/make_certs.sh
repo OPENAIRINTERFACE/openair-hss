ea440a8329062d2192becf6b52b4ce8627e547e3
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2023-01-05 15:15:24 +0100

    feat(ci): support for cgroups2 for target images           push to local private ubuntu registry
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- src/hss_rel14/bin/make_certs.sh
+++ src/hss_rel14/bin/make_certs.sh
@@ -64,14 +64,7 @@ else
   openssl ca -cert cacert.pem -keyfile cakey.pem -in $HOST.csr.pem -out $HOST.cert.pem -outdir . -batch
 fi
 
-IS_CONTAINER=`egrep -c "docker|podman|kubepods|libpod|buildah" /proc/self/cgroup`
-
-if [ $IS_CONTAINER -eq 0 ]
-then
-  SUDO='sudo -S -E'
-else
-  SUDO=''
-fi
+SUDO='sudo -S -E'
 
 $SUDO cp -upv $HOST.cert.pem cacert.pem $HOST.key.pem  $PREFIX/freeDiameter
 
